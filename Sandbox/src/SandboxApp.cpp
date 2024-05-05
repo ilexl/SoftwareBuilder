41cc24e4a7c5a50232997687f182bbf9b6f2f948
Alex <109491531+ilexl@users.noreply.github.com>
2024-05-06 11:52:32 +1200

    Shader Library + misc fixes

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -92,7 +92,7 @@ public:
 			}
 		)";
 
-		m_Shader.reset(SoftwareBuilder::Shader::Create(vertexSrc, fragmentSrc));
+		m_Shader = SoftwareBuilder::Shader::Create("VertexPosColor", vertexSrc, fragmentSrc);
 
 		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
@@ -126,47 +126,14 @@ public:
 			}
 		)";
 
-		m_FlatColorShader.reset(SoftwareBuilder::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
+		m_FlatColorShader = SoftwareBuilder::Shader::Create("FlatColor", flatColorShaderVertexSrc, flatColorShaderFragmentSrc);
 
-		std::string textureShaderVertexSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec2 a_TexCoord;
-
-			uniform mat4 u_ViewProjection;
-			uniform mat4 u_Transform;
-
-			out vec2 v_TexCoord;
-
-			void main()
-			{
-				v_TexCoord = a_TexCoord;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
-			}
-		)";
-
-		std::string textureShaderFragmentSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-
-			in vec2 v_TexCoord;
-			
-			uniform sampler2D u_Texture;
-
-			void main()
-			{
-				color = texture(u_Texture, v_TexCoord);
-			}
-		)";
-
-		m_TextureShader.reset(SoftwareBuilder::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
+		auto textureShader = m_ShaderLibrary.Load("assets/shaders/Texture.glsl");
 
 		m_Texture = SoftwareBuilder::Texture2D::Create("assets/textures/Checkerboard.png");
 
-		std::dynamic_pointer_cast<SoftwareBuilder::OpenGLShader>(m_TextureShader)->Bind();
-		std::dynamic_pointer_cast<SoftwareBuilder::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
+		std::dynamic_pointer_cast<SoftwareBuilder::OpenGLShader>(textureShader)->Bind();
+		std::dynamic_pointer_cast<SoftwareBuilder::OpenGLShader>(textureShader)->UploadUniformInt("u_Texture", 0);
 	}
 
 	void OnUpdate(SoftwareBuilder::Timestep ts) override
@@ -209,8 +176,10 @@ public:
 			}
 		}
 
+		auto textureShader = m_ShaderLibrary.Get("Texture");
+
 		m_Texture->Bind();
-		SoftwareBuilder::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+		SoftwareBuilder::Renderer::Submit(textureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
 		// Triangle
 		// SoftwareBuilder::Renderer::Submit(m_Shader, m_VertexArray);
@@ -229,13 +198,14 @@ public:
 	{
 	}
 private:
+	SoftwareBuilder::ShaderLibrary m_ShaderLibrary;
 	SoftwareBuilder::Ref<SoftwareBuilder::Shader> m_Shader;
 	SoftwareBuilder::Ref<SoftwareBuilder::VertexArray> m_VertexArray;
 
-	SoftwareBuilder::Ref<SoftwareBuilder::Shader> m_FlatColorShader, m_TextureShader;
+	SoftwareBuilder::Ref<SoftwareBuilder::Shader> m_FlatColorShader;
 	SoftwareBuilder::Ref<SoftwareBuilder::VertexArray> m_SquareVA;
 
-	SoftwareBuilder::Ref<SoftwareBuilder::Texture2D> m_Texture;
+	SoftwareBuilder::Ref<SoftwareBuilder::Texture2D> m_Texture, m_ChernoLogoTexture;
 
 	SoftwareBuilder::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
