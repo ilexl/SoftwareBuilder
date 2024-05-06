796bead22e1a1347ef6c4fdfd81db37140658ffe
Alex <109491531+ilexl@users.noreply.github.com>
2024-05-06 17:34:32 +1200

    Window Resizing

--- SoftwareBuilder/src/Platform/OpenGL/OpenGLRendererAPI.h
+++ SoftwareBuilder/src/Platform/OpenGL/OpenGLRendererAPI.h
@@ -9,6 +9,7 @@ namespace SoftwareBuilder {
 		virtual void Clear() override;
 		virtual void Init() override;
 		virtual void DrawIndexed(const std::shared_ptr<VertexArray>& vertexArray) override;
+		virtual void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
 	};
 }
 
--- SoftwareBuilder/src/SoftwareBuilder/Renderer/Renderer.cpp
+++ SoftwareBuilder/src/SoftwareBuilder/Renderer/Renderer.cpp
@@ -7,6 +7,11 @@ namespace SoftwareBuilder
 {
 	Renderer::SceneData* Renderer::s_SceneData = new Renderer::SceneData;
 
+	void Renderer::OnWindowResize(uint32_t width, uint32_t height)
+	{
+		RenderCommand::SetViewport(0, 0, width, height);
+	}
+
 	void Renderer::BeginScene(OrthographicCamera& camera)
 	{
 		s_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
