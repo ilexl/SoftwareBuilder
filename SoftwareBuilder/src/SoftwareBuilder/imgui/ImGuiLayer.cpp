29688742b33af9bd4dbde3d5b886f1489173aa53
Alex <109491531+ilexl@users.noreply.github.com>
2024-05-07 02:23:30 +1200

    Removed imgui example

--- SoftwareBuilder/src/SoftwareBuilder/imgui/ImGuiLayer.cpp
+++ SoftwareBuilder/src/SoftwareBuilder/imgui/ImGuiLayer.cpp
@@ -84,10 +84,4 @@ namespace SoftwareBuilder {
 			glfwMakeContextCurrent(backup_current_context);
 		}
 	}
-
-	void ImGuiLayer::OnImGuiRender()
-	{
-		static bool show = true;
-		ImGui::ShowDemoWindow(&show);
-	}
 }
